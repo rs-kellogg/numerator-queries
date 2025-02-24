71ec66b59f8ecf21a6800fbb7ca8a9257a8c7555
Frank Limbrock <franklimbrock@users.noreply.github.com>
2025-02-24 10:28:53 -0600

    Update egg_prices.sql
    Removed backticks for cross-platform compatibility

--- redivis/egg_prices.sql
+++ redivis/egg_prices.sql
@@ -13,7 +13,7 @@ trans_batch AS (
     transaction_date, 
     -- For each transaction date, find the latest batch date (converted to date only)
     MAX(DATE(batch_date)) as latest_batch_date
-  FROM `standard_nmr_feed_fact_table`
+  FROM standard_nmr_feed_fact_table
   GROUP BY transaction_date
 ) 
 SELECT 
@@ -27,14 +27,14 @@ SELECT
   APPROX_QUANTILES(t.item_unit_price, 100)[OFFSET(49)] AS median_price,
   -- Calculate upper quartile price using approx quantiles
   APPROX_QUANTILES(t.item_unit_price, 100)[OFFSET(74)] AS uq_price
-FROM `standard_nmr_feed_fact_table` AS t
+FROM standard_nmr_feed_fact_table AS t
 -- Join to ensure we're using the latest batch date for each transaction date
 JOIN trans_batch AS tb
   ON t.transaction_date = tb.transaction_date
   -- Only include fact table rows where the batch_date matches the latest batch date for that transaction_date
   AND DATE(t.batch_date) = tb.latest_batch_date
 -- Join the item table to add item details for each fact record
-JOIN `standard_nmr_feed_item_table` i
+JOIN standard_nmr_feed_item_table i
   ON i.item_id = t.item_id
 -- Join to ensure we're using the latest batch date for each item record
 JOIN item_batch ib
